d92d46faf6273e82e527533b7eca4fef7a92c96f
Grant Watson <gwatson117@gmail.com>
2020-06-19 20:17:45 -0600

    UI changes

--- Bravado/Controllers/EntryController.cs
+++ Bravado/Controllers/EntryController.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace Bravado.Controllers
 {
-    [Authorize]
     public class EntryController : Controller
     {
         private readonly AppDbContext _context;
