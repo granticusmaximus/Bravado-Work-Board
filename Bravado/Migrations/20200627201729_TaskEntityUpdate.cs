1d7f4c8c4b6ae5c58c6f3c7c1c0dbe680f790d5d
Grant Watson <gwatson117@gmail.com>
2020-06-27 14:19:08 -0600

    Update to view and Task entiy

---
+++ Bravado/Migrations/20200627201729_TaskEntityUpdate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bravado.Migrations
+{
+    public partial class TaskEntityUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOpened",
+                table: "Tasks",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Priority",
+                table: "Tasks",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateOpened",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "Priority",
+                table: "Tasks");
+        }
+    }
+}
