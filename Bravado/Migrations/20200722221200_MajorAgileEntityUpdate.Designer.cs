fc39bd65be01d0c1b152d7480cc8aed2b40a1dc1
Grant Watson <gwatson117@gmail.com>
2020-07-22 16:12:55 -0600

    Code First EF update

--- Bravado/Migrations/20200722221200_MajorAgileEntityUpdate.Designer.cs
+++ Bravado/Migrations/20200722221200_MajorAgileEntityUpdate.Designer.cs
@@ -4,14 +4,16 @@ using Bravado.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Bravado.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200722221200_MajorAgileEntityUpdate")]
+    partial class MajorAgileEntityUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
