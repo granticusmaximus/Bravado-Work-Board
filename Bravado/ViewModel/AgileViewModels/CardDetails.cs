b0b91bdd0325441bcc65fe3bdbed6c196aa237f1
Grant Watson <gwatson117@gmail.com>
2020-09-01 21:25:47 -0600

    Update to functionality and ui of work board

--- Bravado/ViewModel/AgileViewModels/CardDetails.cs
+++ Bravado/ViewModel/AgileViewModels/CardDetails.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Bravado.ViewModel.AgileViewModels
@@ -6,8 +7,12 @@ namespace Bravado.ViewModel.AgileViewModels
     public class CardDetails
     {
         public int Id { get; set; }
+        
         public string Contents { get; set; }
+
         public string Notes { get; set; }
+
+        public DateTime DueDate { get; set; }
 
         public int Column { get; set; }
         public List<SelectListItem> Columns { get; set; } = new List<SelectListItem>();
